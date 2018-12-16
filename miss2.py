44cc381768274736c3cb1f82bbe95def874362cf
user <>
2018-12-16 19:00:36 +0900

    aaa

---
+++ miss2.py
@@ -0,0 +1,12 @@
+class shape:
+	def __init__(self,w,l):
+		self.width = w
+		self.len = l
+	def print_size(self):
+		print("{}by{}".format(self.width, self.len))
+
+class square(shape):
+	pass
+
+a_sq = square(20, 25)
+a_sq.print_size()
